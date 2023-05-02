97df1c99143547c987a71d5ecd9e892b166e9db5
Jake Fernandez <jake.fernandezbrizuela@outlook.com>
2023-05-02 13:42:51 -0600

    First Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-undef
+const jsonServer = require( "json-server" ); // importing json-server library
+const server = jsonServer.create();
+const router = jsonServer.router( "db.json" );
+const middleWares = jsonServer.defaults();
+const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
+
+server.use( middleWares );
+server.use( router );
+
+server.listen( port );
